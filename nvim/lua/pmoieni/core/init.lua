3c02e7f084817e82a4e619340b51784952a4a997
Parham Moieni <p.moieni.2006@gmail.com>
2023-10-18 18:42:49 +0330

    move lines

--- nvim/lua/pmoieni/core/init.lua
+++ nvim/lua/pmoieni/core/init.lua
@@ -1,4 +1,5 @@
 require("pmoieni.core.base")
+require("pmoieni.core.keymaps")
 require("pmoieni.core.highlights")
 require("pmoieni.core.globals")
 
@@ -8,11 +9,11 @@ local is_win = has "win32"
 local is_wsl = has "wsl"
 
 if is_linux == 1 then
-    require('pmoieni.core.linux')
+	require('pmoieni.core.linux')
 end
 if is_win == 1 then
-    require('pmoieni.core.windows')
+	require('pmoieni.core.windows')
 end
 if is_wsl == 1 then
-    require('pmoieni.core.wsl')
+	require('pmoieni.core.wsl')
 end
