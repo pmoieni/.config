1d577c9f06c7fa721bf20af6b2fb456f731bf30a
Parham Moieni <p.moieni.2006@gmail.com>
2023-10-19 16:23:25 +0330

    various fixes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,10 +6,26 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+local mux = wezterm.mux
+
+wezterm.on('gui-attached', function(domain)
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
+
 config.font = wezterm.font('FiraCode Nerd Font')
 config.font_size = 11.0
 
-config.default_prog = { '/usr/bin/fish', '-l', '-c', 'tmux', 'new-session', '-A' }
+config.window_background_opacity = 0.9
+
+config.color_scheme = 'Catppuccin Mocha'
+
+config.default_prog = { '/usr/bin/fish', '-l', '-c', 'tmux new-session -A' }
 
 config.window_padding = {
 	left = 0,
@@ -21,5 +37,10 @@ config.window_padding = {
 config.enable_tab_bar = false
 
 config.disable_default_key_bindings = true
+
+config.set_environment_variables = {
+	TERM = 'xterm-256color'
+}
+
 
 return config
